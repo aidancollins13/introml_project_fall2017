6ba032f24a1d6b919d8ab5407a05d1c1a6003197
Kim <kimmykong123@gmail.com>
2017-12-17 16:32:54 -0500

    categorical to numerical

--- kim.py
+++ kim.py
@@ -16,6 +16,13 @@ df = df.replace('', 0,regex=True)
 
 df['Reported Date'] = pd.to_datetime(df['Reported Date'])
 
+# df['UNSD Geographical Grouping'] = pd.Categorical(df['UNSD Geographical Grouping']).codes
+# `
+
+df['UNSD Geographical Grouping'], geoIndex = pd.Series(df['UNSD Geographical Grouping']).factorize()
+df['Migrant Route'], migrantIndex = pd.Series(df['Migrant Route']).factorize()
+df['Region of Incident'], regionIndex = pd.Series(df['Region of Incident']).factorize()
+
 # fourteen = df[(df['Reported Date'] >= '2014-01-01') & (df['Reported Date'] < '2015-01-01')]
 print("here")
 print("here")
